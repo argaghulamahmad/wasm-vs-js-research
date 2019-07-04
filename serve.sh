fd24e6726733a590937baccb991ab3ea0c4d04e4
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-07-04 16:46:22 +0700

    Add http server bash script

---
+++ serve.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+python -m SimpleHTTPServer 3002
+
+google-chrome http://localhost:3002/resources/research/js-scanner/demo/index.html
+google-chrome http://localhost:3002/resources/research/wasm-scanner/demo/index.html
