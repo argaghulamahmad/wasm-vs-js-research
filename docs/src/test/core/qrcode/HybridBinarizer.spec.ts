fa24d740d833c689ea46e1916ec24c3e7b9838b5
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-07-19 18:03:12 +0700

    Add docs folder for deployment purpose

---
+++ docs/src/test/core/qrcode/HybridBinarizer.spec.ts
@@ -0,0 +1,31 @@
+import * as assert from 'assert';
+import HybridBinarizer from '../../../core/common/HybridBinarizer';
+import SharpImageLuminanceSource from '../SharpImageLuminanceSource';
+import SharpImage from '../util/SharpImage';
+
+const path = require('path');
+
+describe('HybridBinarizer', () => {
+    it('testHybridBinarizer', done => {
+
+        const pathString = path.resolve('src/test/resources/blackbox/common/simple.png');
+
+        SharpImage.loadWithRotation(pathString, 0)
+            .then(image => {
+
+
+                const source = new SharpImageLuminanceSource(image);
+                const test = new HybridBinarizer(source);
+                const matrix = test.getBlackMatrix();
+
+                assert.equal(0, matrix.get(13, 12));
+                assert.equal(1, matrix.get(13, 13));
+
+                done();
+            })
+            .catch(err => {
+                assert.ok(false, err);
+                done(err);
+            });
+    });
+});
